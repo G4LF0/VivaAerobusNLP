84254e005cb65e17a36dd7b4f27af1350f9cb2f6
Francisco Leonid Galvez Flores <40269728+G4LF0@users.noreply.github.com>
2023-05-09 00:26:54 -0600

    Python files

---
+++ categories.py
@@ -0,0 +1,186 @@
+categories = {
+    'Pagos': ['confirmacion', 'cancelacion', 'pago', 'pagos', 'tarjeta', 'banco', 'credito', 'debito', 'TDC', 'tdc', 'rechazo', 'declinado',
+              'Boleto', 'Pasaje', 'Asiento', 'Ventanilla', 'Pasillo', 'Facturación', 'Equipaje', 'Sobrepeso', 'Maleta', 'Mochila', 
+              'Bolsa de viaje', 'Pago', 'Efectivo', 'Tarjeta de crédito', 'Tarjeta de débito', 'Transferencia bancaria', 'PayPal', 'Google Pay',
+              'Apple Pay', 'Moneda local', 'Cambio de moneda', 'Tasa de cambio', 'Impuestos', 'Seguro de viaje', 'Seguro de cancelación',
+              'Seguro de equipaje', 'Seguro médico', 'Descuento', 'Oferta', 'Promoción', 'Cupón', 'Código de descuento', 'Equipaje perdido', 'Carga',
+              'Tarifa de carga', 'Espacio de carga'],
+
+    'Precios': ['caro', 'costoso', 'asientos', 'tarifas', 'tarifa', 'precio', 'precios', 'equipaje', 'peso', 'sobrepeso', 'Asiento', 'Ventanilla',
+                'Pasillo', 'Equipaje', 'Sobrepeso', 'Maleta', 'Mochila', 'Bolsa de viaje', 'Tarifa de uso del aeropuerto', 'tua', 'barato' ],
+
+    'Técnicos': ['errores', 'carga', 'traba', 'trabado', 'trabada', 'idioma'], # Web
+    
+    'Operaciones': ['conexion', 'escala', 'escalas', 'vuelo', 'vuelos', 'retraso', 'retrasos', 'cancelacion', 'cancelaciones',
+                    'cancelado', 'cancelada', 'cancelados', 'canceladas', 'demora', 'demoras', 'demorado', 'demorada', 'demorados',
+                    'demoradas', 'demorar', 'demore', 'demoren', 'accesibilidad'],
+
+    'Reservacion': ['cambios', 'reserva', 'asiento', 'asientos', 'reasignacion', 'cambio', 'Clase', 'Asiento', 'Ventanilla', 'Pasillo',  'Equipaje',
+                    'Sobrepeso', 'Maleta', 'Mochila', 'Bolsa de viaje', 'Confirmación de reserva', 'Cancelación de reserva', 'Cambio de reserva', 'Reembolso',
+                    'Política de cancelación', 'Política de cambio', 'Política de reembolso', 'Política de equipaje', 'Tamaño del equipaje', 'Peso del equipaje',
+                    'Equipaje de mano', 'Equipaje documentado', 'mascota'],
+
+    'Personal': ['Desinterés', 'Impaciencia', 'Arrogancia', 'Desgano', 'Indiferencia', 'Hostilidad', 'Desorganización', 'Indolencia', 'Antipatía',
+                 'Insolencia', 'Descortesía', 'Falta de empatía', 'Impuntualidad', 'Intransigencia', 'Falta de compromiso', 'Despreocupación', 'Indecisión',
+                 'Falta de cooperación', 'Desprestigio', 'Falta de comunicación', 'Inflexibilidad', 'Desconfianza', 'Deshonestidad', 'Falta de iniciativa',
+                 'Insensibilidad', 'Prepotencia', 'Desconcentración', 'Inatención', 'Desmotivación', 'Falta de respeto', 'Desconocimiento del trabajo',
+                 'Falta de ética', 'Falta de entusiasmo', 'Falta de paciencia', 'Falta de sinceridad', 'Inflexibilidad en políticas',
+                 'Incapacidad para resolver problemas', 'Falta de soluciones', 'Mala actitud hacia el trabajo', 'Falta de atención en detalles',
+                 'Desorden en tareas', 'Falta de habilidad para trabajar bajo presión', 'Falta de capacidad de trabajo en equipo', 'Desinteresado',
+                 'Impuntual', 'Descortés', 'Incompetente', 'Desorganizado', 'Desatento', 'Arrogante', 'Descuidado', 'Maleducado', 'Insensible', 'Irresponsable',
+                 'Inflexible', 'Indiferente', 'Indolente', 'Despreocupado', 'Desmotivado', 'Despreparado', 'Malhumorado', 'Desagradable', 'Indeciso',
+                 'Desconsiderado', 'Desleal', 'Insuficiente', 'Incumplido', 'Irrespetuoso', 'Desesperado', 'Deshonesto', 'Desaliñado', 'Desmotivante',
+                 'Desmotivador', 'Indiscreto', 'Indignado', 'Desenfocado', 'Desatendido', 'Desentendido', 'Despistado', 'Desaprensivo', 'Desobligante',
+                 'Descarado', 'Deshonrado', 'Desobediente', 'Atento', 'Cortés', 'Amable', 'Empático', 'Servicial', 'Proactivo', 'Colaborativo', 'Responsable',
+                 'Detallista', 'Preciso', 'Consciente', 'Creativo', 'Disponible', 'Espontáneo', 'Generoso', 'Innovador', 'Interesado', 'Inspirador', 'Leal',
+                 'Seguro', 'Tranquilo', 'Orientado al cliente', 'Positivo', 'Optimista', 'Emprendedor', 'Reflexivo', 'Solidario', 'Comunicativo', 'Trabajador',
+                 'Confiado', 'Visionario', 'Dinámico', 'Persistente', 'Flexible', 'Convincente', 'Confiable', 'Alegre', 'Comprensivo', 'Paciente', 'Eficiente',
+                 'Profesional', 'Sincero', 'Agradable', 'Respetuoso', 'Entusiasta', 'Organizado', 'Innovador', 'Interesado', 'Inspirador', 'Solidario'],
+
+    'Aeropuerto': ['aeropuerto', 'seguridad', 'Transporte de equipaje', 'perdida', 'Equipaje retrasado', 'control', 'aduana', 'inmigracion', 'migracion', 'sala de espera', 'sala de abordar', 'capilla'],
+
+    'Servicios': ['comida', 'a bordo', 'servicio',  'Wi-Fi', 'wifi', 'internet', 'bebidas', 'refrescos', 'jugos', 'fria', 'caliente', 'sabor', 'rica', 'mala', 'baños', 'luces', 'enchufes', 'enchufe', 'pantalla', 'entretenimiento'],
+
+    'Experiencias': ['tiempo'],
+
+    'Check-in': ['pase', 'pase de abordar', 'abordar', 'abordaje', 'check-in', 'checkin'],
+
+    'Otros': ['identificacion', 'accesibilidad', 'discapacidad', 'movilidad', 'discapacitado', 'dicapacitada', 'discapacitados', 'documentacion']
+}
+
+x = '''Desinterés
+Impaciencia
+Arrogancia
+Desgano
+Indiferencia
+Hostilidad
+Desorganización
+Indolencia
+Antipatía
+Insolencia
+Descortesía
+Falta de empatía
+Impuntualidad
+Intransigencia
+Falta de compromiso
+Despreocupación
+Indecisión
+Falta de cooperación
+Desprestigio
+Falta de comunicación
+Inflexibilidad
+Desconfianza
+Deshonestidad
+Falta de iniciativa
+Insensibilidad
+Prepotencia
+Desconcentración
+Inatención
+Desmotivación
+Falta de respeto
+Desconocimiento del trabajo
+Falta de ética
+Falta de entusiasmo
+Falta de paciencia
+Falta de sinceridad
+Inflexibilidad en políticas
+Incapacidad para resolver problemas
+Falta de soluciones
+Mala actitud hacia el trabajo
+Falta de atención en detalles
+Desorden en tareas
+Falta de habilidad para trabajar bajo presión
+Falta de capacidad de trabajo en equipo
+Desinteresado
+Impuntual
+Descortés
+Incompetente
+Desorganizado
+Desatento
+Arrogante
+Descuidado
+Maleducado
+Insensible
+Irresponsable
+Inflexible
+Indiferente
+Indolente
+Despreocupado
+Desmotivado
+Despreparado
+Malhumorado
+Desagradable
+Indeciso
+Desconsiderado
+Desleal
+Insuficiente
+Incumplido
+Irrespetuoso
+Desesperado
+Deshonesto
+Desaliñado
+Desmotivante
+Desmotivador
+Indiscreto
+Indignado
+Desenfocado
+Desatendido
+Desentendido
+Despistado
+Desaprensivo
+Desobligante
+Descarado
+Deshonrado
+Desobediente
+Atento
+Cortés
+Amable
+Empático
+Servicial
+Proactivo
+Colaborativo
+Responsable
+Detallista
+Preciso
+Consciente
+Creativo
+Disponible
+Espontáneo
+Generoso
+Innovador
+Interesado
+Inspirador
+Leal
+Seguro
+Tranquilo
+Orientado al cliente
+Positivo
+Optimista
+Emprendedor
+Reflexivo
+Solidario
+Comunicativo
+Trabajador
+Confiado
+Visionario
+Dinámico
+Persistente
+Flexible
+Convincente
+Confiable
+Alegre
+Comprensivo
+Paciente
+Eficiente
+Profesional
+Sincero
+Agradable
+Respetuoso
+Entusiasta
+Organizado
+Innovador
+Interesado
+Inspirador
+Solidario'''
+
+print(list(x.replace('\n', ', ').split(', ')))
